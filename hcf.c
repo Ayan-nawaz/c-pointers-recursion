8d475b47d3740f5f92e304bb745a7a04fec3bce9
Ayan Nawaz <ayaankhan809253@gmail.com>
2026-07-13 13:33:00 +0530

    Add C pointer and recursion programs

---
+++ hcf.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int min(int a, int b) {
+    if (a < b)
+        return a;
+    else
+        return b;
+}
+
+int gcd(int a, int b) {
+    int hcf;
+
+    for (int i = min(a, b); i >= 1; i--) {
+        if (a % i == 0 && b % i == 0) {
+            hcf = i;
+            break;
+        }
+    }
+
+    return hcf;
+}
+
+int main() {
+    int a, b;
+
+    printf("Enter 1st number: ");
+    scanf("%d", &a);
+
+    printf("Enter 2nd number: ");
+    scanf("%d", &b);
+
+    printf("GCD = %d\n", gcd(a, b));
+
+    return 0;
+}
